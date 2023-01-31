bb777ee1f498d98ce9c0dbdbc3fac88aac202707
Robin <robin.james.guerrier@gmail.com>
2023-01-30 20:00:11 -0500

    Add get all phonebook data response.

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
+const phonebookData = [
+    { 
+      "name": "Arto Hellas", 
+      "phoneNumber": "040-123-0456"
+    },
+    { 
+      "name": "Ada Lovelace", 
+      "phoneNumber": "395-443-3523"
+    },
+    { 
+      "name": "Dan Abramov", 
+      "phoneNumber": "122-433-4345"
+    },
+    { 
+      "name": "Mary Poppendieck", 
+      "phoneNumber": "396-234-3122"
+    }
+];
+//################################################
+
 const express = require('express');
 const app = express();
-const PORT = 3003;
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World</h1>');
 });
+
+app.get('/api/phonebook', (request, response) => {
+    response.send(phonebookData);
+});
+
+
+
+//################## run server ##################
+const PORT = 3003;
 
 app.listen(PORT, () => {
     console.log(`Starting server on port ${PORT}...`);
